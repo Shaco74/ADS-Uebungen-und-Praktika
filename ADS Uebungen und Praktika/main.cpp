b56fd868367645e2fde3289edfa97ceac2ee9237
kacper.coemlek <kacper.coemlek@kanzleipilot.de>
2023-03-20 16:07:23 +0100

    Initial Commit

---
+++ ADS Uebungen und Praktika/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ADS Uebungen und Praktika
+//
+//  Created by Kacper Cömlek on 20.03.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
